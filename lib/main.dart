be92e9cd2ec2bc4064daac0cd6f33f9febaf0001
arijeet <arijeetc6@gmail.com>
2023-11-18 17:20:51 +0530

    Init commit (Added provider package)

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:e_commerce_app/pages/splash_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => null,
+      child: MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const SplashPage(),
+      ),
+    );
+  }
+}
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Text('Splash Page'),
+        ),
+      ),
+    );
+  }
+}
