cd5f399a938de8afa147099b5481c240beed35a2
arijeet <arijeetc6@gmail.com>
2023-11-18 20:51:38 +0530

    Added delay while going from splash to cart screen.

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,38 +1,59 @@
+import 'package:e_commerce_app/pages/shop_page.dart';
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
   static const routeName = '/splash';
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    goToShopsPage();
+  }
+
+  goToShopsPage() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushNamed(context, ShopPage.routeName);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Icon(
-              Icons.trolley,
-              size: 72,
-              color: Theme.of(context).colorScheme.primary,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'SHOP-SHOP',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              'Your one stop destination for everything..',
-              style: TextStyle(
-                fontWeight: FontWeight.w200,
-                fontSize: 16,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.trolley,
+                size: 72,
+                color: Theme.of(context).colorScheme.primary,
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'SHOP-SHOP',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Your one stop destination for everything..',
+                style: TextStyle(
+                  fontWeight: FontWeight.w200,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
